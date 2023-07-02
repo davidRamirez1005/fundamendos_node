95ab5279f20f55798a9b00511239e5bb22530229
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-07-02 13:02:26 -0500

    feat: :sparkles: require
    explicacion y ejemplo de uso

---
+++ require/app.js
@@ -0,0 +1,11 @@
+const datos = require('./datos.json')
+
+console.log('====================================');
+console.log("json :",datos);
+console.log('====================================');
+console.log('====================================');
+console.log("json id: ",datos.campus.map(res => res.id).join('//'));
+console.log('====================================');
+console.log('====================================');
+console.log("json name: ",datos.campus.map(res => res.name).join('//'));
+console.log('====================================');
