7e9a3d4db2c8cbae9adaffe0dfaa6d056862f893
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-07-04 17:30:07 -0500

    feat: :zap:

--- fs/fyleSistem.js
+++ fs/fyleSistem.js
@@ -1,19 +1,19 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 
 
 async function leer(path) {
-    const data = await fs.promises.readFile(path);
+    const data = await fs.readFile(path);
     return data.toString();
 }
 async function agregarContenido(path, contenido) {
-    await fs.promises.appendFile(path, contenido);
+    await fs.appendFile(path, contenido);
     console.log('Contenido agregado exitosamente al archivo');
 }
 async function eliminar (path) {
     fs.unlinkSync(path)
 }
 async function editar(path, contenido) {
-    await fs.promises.writeFile(path, contenido);
+    await fs.writeFile(path, contenido);
     console.log('Archivo editado exitosamente');
 }
 
