2501e686a0edec5cacd31c5f97565d02730a07a4
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-07-04 11:07:29 -0500

    feat: :sparkles: cowsay

---
+++ modulos/cowsay.js
@@ -0,0 +1,48 @@
+const cowsay = require('cowsay')
+
+
+console.log(cowsay.say({
+ text: ':o que interesante esto jajaja ._. soy un a nimal!',
+ f : 'dragon-and-cow'
+}))
+
+
+// 'beavis.zen'
+// 'bong'
+// 'bud-frogs'
+// 'bunny'
+// 'cheese'
+// 'cower'
+// 'daemon'
+// 'dragon-and-cow'
+// 'dragon'
+// 'elephant-in-snake'
+// 'elephant'
+// 'eyes'
+// 'flaming-sheep'
+// 'ghostbusters'
+// 'head-in'
+// 'kiss'
+// 'kitty'
+// 'mech-and-cow'
+// 'meow'
+// 'milk'
+// 'moofasa'
+// 'moose'
+// 'ren'
+// 'satanic'
+// 'sheep'
+// 'skeleton'
+// 'small'
+// 'sodomized'
+// 'squirrel'
+// 'stegosaurus'
+// 'stimpy'
+// 'supermilker'
+// 'surgery'
+// 'telebears'
+// 'turkey'
+// 'turtle'
+// 'tux'
+// 'vader-koala'
+// 'vader'
