8e5e7d6ef0c6bf809aad8351440fb5e21988bc8d
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-07-04 01:26:44 -0500

    feat: :sparkles: montar router

---
+++ http/servidor.js
@@ -0,0 +1,22 @@
+const http = require(`http`);
+
+http.createServer(theServer).listen(3005);
+
+function theServer(req, res) {
+    console.log(`nueva petición`);
+    console.log(req.url);
+    switch (req.url) {
+        case `/hola`:
+            res.write("Bienvenido a hola");
+            res.end();
+            break;
+        case `/node`:
+            res.write("Comienza a aprender NodeJs");
+            res.end();
+            break;
+        default:
+            res.write("Error 404 Page No Found");
+            res.end();
+            break;
+    }
+}
