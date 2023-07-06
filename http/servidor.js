4588102f04948596f12456b0f14915d770ef4696
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-07-06 09:12:59 -0500

    feat: :zap: inclullendo await
    y cambiando ruta de readfile

--- http/servidor.js
+++ http/servidor.js
@@ -1,24 +1,35 @@
-import {createServer} from "http";
-import fs from "fs";
+import { createServer } from "http";
+import { readFile } from "fs/promises";
 
-const appHttp = createServer((req, res) => {
-    if(req.url == "/"){
-        fs.readFile("./index.html","utf-8", (err, data)=>{
-            console.log(data);
-            res.end();
-        })
-    }else if (req.url == "/home"){
-        fs.readFile("./prueba.html","utf-8", (err, data)=>{
-            res.end(data);
-        })
+const appHttp = createServer(async (req, res) => {
+  if (req.url == "/") {
+    try {
+      const data = await readFile("http/index.html", "utf-8");
+      console.log(data);
+      res.end(data);
+    } catch (err) {
+      console.error(err);
+      res.statusCode = 500;
+      res.end("Error 500");
     }
+  } 
+  else if (req.url == "/home") {
+    try {
+      const data = await readFile("http/prueba.html", "utf-8");
+      res.end(data);
+    } catch (err) {
+      console.error(err);
+      res.statusCode = 500;
+      res.end("Error 500");
+    }
+  }
 });
 
-const config ={
-    hostname: "127.60.70.20",
-    port: 5015
+const config = {
+  hostname: "127.60.70.20",
+  port: 5015
 };
 
 appHttp.listen(config, () => {
-    console.log(`http://${config.hostname}:${config.port}/`);
+  console.log(`http://${config.hostname}:${config.port}/`);
 });
